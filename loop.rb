5c11c59cb756f57d10e59198460ee091ca751ebc
Diego <diego-ii@live.com>
2014-09-04 18:17:09 -0300

    changed everything

--- loop.rb
+++ loop.rb
@@ -1,39 +1,24 @@
 loop do 
-
-	puts "1 Bem Vindo"
  	
- 	puts "2 Opções de Escolha"
+ 	puts "1 Limpeza"
  	
- 	puts "3 Limpeza"
- 	
- 	puts "4 Higiene Pessoal"
+ 	puts "2 Higiene Pessoal"
  	 
- 	puts "5 Alimentos"
+ 	puts "3 Alimentos"
 
 
-	option = gets.chomp()
-	
+ 
 	if (option.to_i == 1)
-		system "clear"
-		puts "Bem Vindo"
-		gets()
-	end
-	if (option.to_i == 2)
-		system "clear"
-		puts "Opções de Escolha"
-		gets()
-	end 
-	if (option.to_i == 3)
 		system "clear"
 		puts "Limpeza"
 		gets()
 	end
-	if (option.to_i == 4)
+	if (option.to_i == 2)
 		system "clear"
 		puts "Higiene Pessoal"
 		gets()
 	end
-	if (option.to_i == 5)
+	if (option.to_i == 3)
 		system "clear"
 		puts "Alimentos"
 		gets()
